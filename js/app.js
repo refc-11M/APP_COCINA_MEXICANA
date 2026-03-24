ee8260de8a9d1544f20a0790fcd831a0a6d02fa0
refc-11M <refc070811mmcybta9@soycecytem.mx>
2026-03-24 08:46:59 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 
+function mostrarSeccion(id) {
+  let secciones = document.querySelectorAll(".seccion");
+  secciones.forEach(function(seccion) {
+    seccion.classList.remove("activa");
+  });
+  document.getElementById(id).classList.add("activa");
+}
